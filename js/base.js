346974148eae4895c39a1066e280b33e06c6576f
Hoang-1910 <bhoang1910@gmail.com>
2024-07-08 14:38:31 +0700

    update

--- js/base.js
+++ js/base.js
@@ -1,3 +1,11 @@
-
-
- 
+// Cố định phần header khi cuộn xuống
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if($(this).scrollTop()){
+            $('header').addClass('sticky');
+        }
+        else {
+            $('header').removeClass('sticky');
+        }
+    });
+});
